f1c968011b8b3d9248b33182eb599ff705fff318
shineeshbosco <sshineeshbosco@gmail.com>
2025-02-12 11:20:42 +0530

    Add files via upload

---
+++ matlotlib2.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+x=[1,2,3,4,5]
+y=[2,4,6,8,10]
+plt.plot(x,y,color='red',marker='*',mec='g',ms='20')
+plt.show()
---
+++ matlotlib3.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+x=[1,2,3,4,5]
+y=[2,4,6,8,10]
+
+plt.plot(x,y, linestyle = 'dotted')
+plt.show()
