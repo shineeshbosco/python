4124a18a0b53619aba93e98a800feeefddd50662
shineeshbosco <sshineeshbosco@gmail.com>
2025-02-17 11:11:54 +0530

    Add files via upload

---
+++ subplot.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+x = [0, 1, 2, 3]
+y = [3, 8, 1, 10]
+
+plt.subplot(1, 2, 1)
+plt.plot(x,y)
+
+x =[0, 1, 2, 3]
+y =[10, 20, 30, 40]
+
+plt.subplot(1, 2, 2)
+plt.plot(x,y)
+plt.show()
